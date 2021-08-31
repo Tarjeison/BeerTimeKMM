ee69f8accf8f32818297a98fca3ffb14c57bd455
Trym Tarjeison Lekva <Tarjeison@gmail.com>
2021-08-31 23:09:02 +0200

    Created an CountDownClockImpl.kt for iOS

--- shared/src/iosMain/kotlin/com/tlapp/beertimemm/utils/CountDownClockImpl.kt
+++ shared/src/iosMain/kotlin/com/tlapp/beertimemm/utils/CountDownClockImpl.kt
@@ -1,13 +1,35 @@
 package com.tlapp.beertimemm.utils
 
+import platform.Foundation.NSTimer
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
+private const val oneSecond: Double = 1.0
+
 @ExperimentalTime
 actual class CountDownClockImpl : CountDownClock {
-    actual override suspend fun countDownEachSecond(duration: Duration, onTick: (millisUntilFinished: Long) -> Unit, onFinished: () -> Unit) {
+    private var countDownTimer: NSTimer? = null
+    private var remainingDurationInMs = -1L
+
+    actual override suspend fun countDownEachSecond(
+        duration: Duration,
+        onTick: (millisUntilFinished: Long) -> Unit,
+        onFinished: () -> Unit
+    ) {
+        countDownTimer?.invalidate()
+        remainingDurationInMs = duration.inWholeMilliseconds
+        countDownTimer = NSTimer.scheduledTimerWithTimeInterval(oneSecond, false) {
+            remainingDurationInMs -= 1000L
+            if (remainingDurationInMs < 0L) {
+                onFinished.invoke()
+            } else {
+                onTick.invoke(remainingDurationInMs)
+            }
+        }
     }
 
     actual override fun stop() {
+        countDownTimer?.invalidate()
+        countDownTimer = null
     }
 }

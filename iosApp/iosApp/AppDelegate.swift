f73cc89b5f309f0b8ff668ca3232799240ba78f1
Trym Tarjeison Lekva <Tarjeison@gmail.com>
2021-09-28 22:48:34 +0200

    Fixed some iOS 15 navigationBar breaking changes, apple plz

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -13,23 +13,36 @@ import shared
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions
-        launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+                     launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         startKoin()
         let profileStorage = koin.get(objCClass: ProfileStorage.self) as! ProfileStorage
         profileStorage.saveUserProfile(userProfile: UserProfile(gender: Gender.male, weight: 75))
-
+        
         let tabController = UITabBarController()
         tabController.view.backgroundColor = .white
         let startDrinkingViewNavController = UINavigationController(rootViewController: StartDrinkingViewController())
         let countDownViewNavController = UINavigationController(rootViewController: CountDownViewController())
         let profileViewNavController = UINavigationController(rootViewController: MePageViewController())
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(named: "Orange")
         startDrinkingViewNavController.navigationBar.barTintColor = UIColor(named: "Orange")
+        startDrinkingViewNavController.navigationBar.standardAppearance = appearance
+        startDrinkingViewNavController.navigationBar.scrollEdgeAppearance = startDrinkingViewNavController.navigationBar.standardAppearance
+        
         countDownViewNavController.navigationBar.barTintColor = UIColor(named: "Orange")
+        countDownViewNavController.navigationBar.standardAppearance = appearance
+        countDownViewNavController.navigationBar.scrollEdgeAppearance = countDownViewNavController.navigationBar.standardAppearance
+        
         profileViewNavController.navigationBar.barTintColor = UIColor(named: "Orange")
+        profileViewNavController.navigationBar.standardAppearance = appearance
+        profileViewNavController.navigationBar.scrollEdgeAppearance =
+        profileViewNavController.navigationBar.standardAppearance
+        
         startDrinkingViewNavController.title = "Start drinking"
         countDownViewNavController.title = "Current status"
         profileViewNavController.title = "Profile"
